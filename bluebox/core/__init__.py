950ee83ed3dcd6aee8fb513a8f60cc00c0ae90f5
Joaquin9999 <joaquin.castano@upb.edu.co>
2026-03-18 21:30:21 -0500

    feat: add case classification engine and cli command

--- bluebox/core/__init__.py
+++ bluebox/core/__init__.py
@@ -5,14 +5,17 @@ from .case_model import (
     ensure_case_structure,
     sanitize_case_name,
 )
+from .classification import ClassificationOutcome, classify_case
 from .init_service import initialize_case_from_artifacts
 from .validation import ALLOWED_SOLUTION_STATUS, ValidationReport, validate_case_structure
 from .workspace_builder import create_case_workspace
 
 __all__ = [
     "CaseWorkspaceSpec",
+    "ClassificationOutcome",
     "REQUIRED_CASE_DIRECTORIES",
     "REQUIRED_CASE_FILES",
+    "classify_case",
     "sanitize_case_name",
     "ensure_case_structure",
     "create_case_workspace",
